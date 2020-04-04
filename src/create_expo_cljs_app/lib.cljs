438170221048c35d1a58ef006d0289f02489017c
Justin Good <jgoodhcg@gmail.com>
2020-04-04 13:46:37 -0400

    Silently install expo deps and refactor commands

--- src/create_expo_cljs_app/lib.cljs
+++ src/create_expo_cljs_app/lib.cljs
@@ -1,23 +1,22 @@
 (ns create-expo-cljs-app.lib
   (:require [create-cljs-app.lib :as cca-lib]
-            [create-cljs-app.utils :refer
-             [should-use-yarn? has-binary-on-PATH?]]
-            ["chalk" :refer [blue green red yellow]]
-            ["path" :refer [basename join]]
-            ["shelljs" :refer [exec]]))
+            [create-cljs-app.utils :refer [has-binary-on-PATH?]]
+            ["chalk" :refer [blue green red yellow]]))
 
 (defn get-commands
   [use-yarn]
   (if use-yarn
-    {:install "yarn"
+    {:install "yarn && expo install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context @react-native-community/masked-view"
      :start   "yarn start"
      :shadow  "shadow-cljs watch app"}
     {:install "npm install"
-     :start   "npm start"
-     :shadow  "shadow-cljs watch app"}))
+     :start   "npm start && expo install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context @react-native-community/masked-view"
+     :shadow  "shadow-cljs watch app"
+     :expo    "expo install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context @react-native-community/masked-view
+"}))
 
 (defn done-msg
-  [commands name path abs-path install-failed?]
+  [name path abs-path commands install-failed?]
   (.log
    js/console
    (str
@@ -39,23 +38,16 @@ We suggest that you begin by: \n  "
     (blue (:start commands)) "\n\n"
     "Happy hacking! \n")))
 
-(defn install-expo-managed-deps []
-  (if (has-binary-on-PATH? "expo")
-    (exec "expo install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context @react-native-community/masked-view
-")
-    (.log js/console (str (red "Missing expo-cli tool!") "\n"
-                          (red "Installation instructions here: https://docs.expo.io/versions/latest/workflow/expo-cli/#installation") "\n"
-                          (yellow "Afterwards please install expo managed dependencies manually.") "\n"
-                          (yellow "Installation instructions here: https://reactnavigation.org/docs/getting-started/#installing-dependencies-into-an-expo-managed-project")))))
+(defn expo-cli-warning []
+  (.log js/console (str (red "Missing expo-cli tool!") "\n"
+                        (red "Installation instructions here: https://docs.expo.io/versions/latest/workflow/expo-cli/#installation") "\n"
+                        (yellow "Afterwards please install expo managed dependencies manually.") "\n"
+                        (yellow "Installation instructions here: https://reactnavigation.org/docs/getting-started/#installing-dependencies-into-an-expo-managed-project"))))
 
 (defn create [cwd path]
-
-  (let [abs-path (join cwd path)
-        name     (basename abs-path)
-        use-yarn (should-use-yarn?)
-        commands (get-commands use-yarn)]
-
-    (cca-lib/create cwd path {:done-msg       (partial done-msg commands)
-                              :install-extras install-expo-managed-deps})))
+  (cca-lib/create cwd path {:done-msg       done-msg
+                            :get-commands   get-commands
+                            :has-extras?    (fn [] (has-binary-on-PATH? "expo"))
+                            :extras-warning expo-cli-warning}))
 
 (def exports #js {:create create})
