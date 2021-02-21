7794f6344141241c91d3fa838fffd0017768f9b8
justin <jgoodhcg@gmail.com>
2021-02-21 11:57:25 -0500

    Add a few libs and clean up index

--- template/src/app/index.cljs
+++ template/src/app/index.cljs
@@ -1,47 +1,38 @@
 (ns app.index
   (:require
+   ["@react-navigation/native" :as nav]
+   ["@react-navigation/stack" :as rn-stack]
    ["expo" :as ex]
    ["expo-constants" :as expo-constants]
-   ["react-native" :as rn]
    ["react" :as react]
-   ["@react-navigation/native" :as nav]
-   ["@react-navigation/bottom-tabs" :as bottom-tabs]
+   ["react-native" :as rn]
    ["react-native-paper" :as paper]
+   ["tailwind-rn" :default tailwind-rn]
+
    [applied-science.js-interop :as j]
    [camel-snake-kebab.core :as csk]
    [camel-snake-kebab.extras :as cske]
    [reagent.core :as r]
-   [re-frame.core :refer [subscribe dispatch dispatch-sync]]
+   [re-frame.core :refer [dispatch-sync]]
    [shadow.expo :as expo]
+
    [app.fx]
    [app.handlers]
    [app.subscriptions]
    [app.helpers :refer [<sub >evt]]))
+
+(defn tw [style-str]
+  ;; https://github.com/vadimdemedes/tailwind-rn#supported-utilities
+  (-> style-str
+      tailwind-rn
+      (js->clj :keywordize-keys true)))
 
 ;; must use defonce and must refresh full app so metro can fill these in
 ;; at live-reload time `require` does not exist and will cause errors
 ;; must use path relative to :output-dir
 (defonce splash-img (js/require "../assets/shadow-cljs.png"))
 
-(def styles
-  ^js (-> {:surface
-           {:flex            1
-            :justify-content "center"}
-
-           :theme-switch
-           {:flex-direction  "row"
-            :justify-content "space-between"}}
-          (#(cske/transform-keys csk/->camelCase %))
-          (clj->js)
-          (rn/StyleSheet.create)))
-
-(defn screen2 [props]
-  (r/as-element
-    [:> paper/Surface {:style (-> styles (j/get :surface))}
-     [:> rn/View
-      [:> paper/Title "Screen2"]]]))
-
-(defn screen1 [props]
+(defn screen-main [props]
   (r/as-element
     (let [version         (<sub [:version])
           theme-selection (<sub [:theme])
@@ -50,29 +41,33 @@
                               (j/get :default)
                               (j/get :manifest)
                               (j/get :sdkVersion))]
-      [:> paper/Surface {:style (-> styles (j/get :surface))}
-       [:> rn/View
-        [:> paper/Card
-         [:> paper/Card.Title {:title    "My new expo cljs app!"
-                               :subtitle (str "Version: " version)}]
-         [:> paper/Card.Content
-          [:> paper/Paragraph (str "Using Expo SDK: " expo-version)]
-          [:> rn/View {:style (-> styles (j/get :themeSwitch))}
-           [:> paper/Text
-            {:style {:color (-> theme
-                                (j/get :colors)
-                                (j/get :accent))}}
-            "Dark mode"]
-           [:> paper/Switch {:value           (= theme-selection :dark)
-                             :on-value-change #(>evt [:set-theme (if (= theme-selection :dark)
-                                                                   :light
-                                                                   :dark)])}]]]]]])))
 
-(def tab (bottom-tabs/createBottomTabNavigator))
+      [:> rn/SafeAreaView {:style (tw "flex flex-1")}
+       [:> rn/StatusBar {:visibility "hidden"}]
+       [:> paper/Surface {:style (tw "flex flex-1 justify-center")}
+        [:> rn/View
+         [:> paper/Card
+          [:> paper/Card.Cover {:source splash-img}]
+          [:> paper/Card.Title {:title    "My new expo cljs app!"
+                                :subtitle (str "Version: " version)}]
+          [:> paper/Card.Content
+           [:> paper/Paragraph (str "Using Expo SDK: " expo-version)]
+           [:> rn/View {:style (tw "flex flex-row justify-between")}
+            [:> paper/Text
+             {:style {:color (-> theme
+                                 (j/get :colors)
+                                 (j/get :accent))}}
+             "Dark mode"]
+            [:> paper/Switch {:value           (= theme-selection :dark)
+                              :on-value-change #(>evt [:set-theme (if (= theme-selection :dark)
+                                                                    :light
+                                                                    :dark)])}]]]]]]])))
 
-(defn tab-navigator [] (-> tab (j/get :Navigator)))
+(def stack (rn-stack/createStackNavigator))
 
-(defn tab-screen [props] [:> (-> tab (j/get :Screen)) props])
+(defn navigator [] (-> stack (j/get :Navigator)))
+
+(defn screen [props] [:> (-> stack (j/get :Screen)) props])
 
 (defn root []
   (let [theme           (<sub [:theme])
@@ -96,16 +91,14 @@
                                 current-route-name (-> @!navigation-ref
                                                        (j/call :getCurrentRoute)
                                                        (j/get :name))]
-                            (if (not= prev-route-name current-route-name)
+                            (when (not= prev-route-name current-route-name)
                               ;; This is where you can do side effecty things like analytics
                               (>evt [:some-fx-example (str "New screen encountered " current-route-name)]))
                             (swap! !route-name-ref merge {:current current-route-name})))}
 
-      [:> (tab-navigator)
-       (tab-screen {:name      "Screen1"
-                    :component (paper/withTheme screen1)})
-       (tab-screen {:name      "Screen2"
-                    :component (paper/withTheme screen2)})]]]))
+      [:> (navigator) {:header-mode "none"}
+       (screen {:name      "Screen1"
+                :component (paper/withTheme screen-main)})]]]))
 
 (defn start
   {:dev/after-load true}
