d29851890d213da1baa15fdc55370d40aa2a5348
justin <jgoodhcg@gmail.com>
2022-03-21 19:40:07 -0400

    Upgrade npm deps for template

--- template/src/app/index.cljs
+++ template/src/app/index.cljs
@@ -6,10 +6,12 @@
    ["expo-constants" :as expo-constants]
    ["react" :as react]
    ["react-native" :as rn]
+   ["react-native-gesture-handler" :as g]
    ["react-native-paper" :as paper]
-   ["tailwind-rn" :default tailwind-rn]
+   ["twrnc" :default twrnc]
 
    [applied-science.js-interop :as j]
+   [clojure.string :refer [split]]
    [reagent.core :as r]
    [re-frame.core :refer [dispatch-sync]]
    [shadow.expo :as expo]
@@ -21,8 +23,9 @@
 
 (defn tw [style-str]
   ;; https://github.com/vadimdemedes/tailwind-rn#supported-utilities
-  (-> style-str
-      tailwind-rn
+  (-> twrnc
+      (j/get :style)
+      (apply (-> style-str (split " ")))
       (js->clj :keywordize-keys true)))
 
 ;; must use defonce and must refresh full app so metro can fill these in
@@ -32,22 +35,22 @@
 
 (defn screen-main [props]
   (r/as-element
-    (let [version         (<sub [:version])
-          theme-selection (<sub [:theme])
-          theme           (-> props (j/get :theme))
-          expo-version    (-> expo-constants
-                              (j/get :default)
-                              (j/get :manifest)
-                              (j/get :sdkVersion))]
+   (let [version         (<sub [:version])
+         theme-selection (<sub [:theme])
+         theme           (-> props (j/get :theme))
+         expo-version    (-> expo-constants
+                             (j/get :default)
+                             (j/get :manifest)
+                             (j/get :sdkVersion))]
 
-      [:> rn/SafeAreaView {:style (tw "flex flex-1")}
-       [:> rn/StatusBar {:visibility "hidden"}]
-       [:> paper/Surface {:style (tw "flex flex-1 justify-center")}
+     [:> rn/SafeAreaView {:style (tw "flex flex-1")}
+      [:> rn/StatusBar {:visibility "hidden"}]
+      [:> paper/Surface {:style (tw "flex flex-1 justify-center")}
         [:> rn/View
          [:> paper/Card
-          [:> paper/Card.Cover {:source splash-img}]
           [:> paper/Card.Title {:title    "My new expo cljs app!"
                                 :subtitle (str "Version: " version)}]
+          [:> paper/Card.Cover {:source splash-img}]
           [:> paper/Card.Content
            [:> paper/Paragraph (str "Using Expo SDK: " expo-version)]
            [:> rn/View {:style (tw "flex flex-row justify-between")}
@@ -63,40 +66,47 @@
 
 (def stack (rn-stack/createStackNavigator))
 
-(defn navigator [] (-> stack (j/get :Navigator)))
+(defn stack-navigator [] (-> stack (j/get :Navigator)))
 
-(defn screen [props] [:> (-> stack (j/get :Screen)) props])
+(defn stack-screen [props] [:> (-> stack (j/get :Screen)) props])
+
+(defn wrap-screen
+  [the-screen]
+  (g/gestureHandlerRootHOC
+   (paper/withTheme the-screen)))
 
 (defn root []
   (let [theme           (<sub [:theme])
         !route-name-ref (clojure.core/atom {})
         !navigation-ref (clojure.core/atom {})]
 
-    [:> paper/Provider
-     {:theme (case theme
-               :light paper/DefaultTheme
-               :dark  paper/DarkTheme
-               paper/DarkTheme)}
+     [:> paper/Provider
+      {:theme (case theme
+                :light paper/DefaultTheme
+                :dark  paper/DarkTheme
+                paper/DarkTheme)}
 
-     [:> nav/NavigationContainer
-      {:ref             (fn [el] (reset! !navigation-ref el))
-       :on-ready        (fn []
-                          (swap! !route-name-ref merge {:current (-> @!navigation-ref
-                                                                     (j/call :getCurrentRoute)
-                                                                     (j/get :name))}))
-       :on-state-change (fn []
-                          (let [prev-route-name    (-> @!route-name-ref :current)
-                                current-route-name (-> @!navigation-ref
-                                                       (j/call :getCurrentRoute)
-                                                       (j/get :name))]
-                            (when (not= prev-route-name current-route-name)
-                              ;; This is where you can do side effecty things like analytics
-                              (>evt [:some-fx-example (str "New screen encountered " current-route-name)]))
-                            (swap! !route-name-ref merge {:current current-route-name})))}
+      [:> nav/NavigationContainer
+       {:ref             (fn [el] (reset! !navigation-ref el))
+        :on-ready        (fn []
+                           (swap! !route-name-ref merge {:current (-> @!navigation-ref
+                                                                      (j/call :getCurrentRoute)
+                                                                      (j/get :name))}))
+        :on-state-change (fn []
+                           (let [prev-route-name    (-> @!route-name-ref :current)
+                                 current-route-name (-> @!navigation-ref
+                                                        (j/call :getCurrentRoute)
+                                                        (j/get :name))]
+                             (when (not= prev-route-name current-route-name)
+                               ;; This is where you can do side effecty things like analytics
+                               (>evt [:some-fx-example (str "New screen encountered " current-route-name)]))
+                             (swap! !route-name-ref merge {:current current-route-name})))}
 
-      [:> (navigator) {:header-mode "none"}
-       (screen {:name      "Screen1"
-                :component (paper/withTheme screen-main)})]]]))
+       [:> (stack-navigator) {:header-mode "none"}
+        (stack-screen {:name      "Screen1"
+                       :component (wrap-screen screen-main)
+                       :options   {}})]]]
+  ))
 
 (defn start
   {:dev/after-load true}
